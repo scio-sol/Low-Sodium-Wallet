934ca4d076007cb9a158ff4ccb319d23d8871d0b
scio-sol <dasasasa000+solidity.projects@gmail.com>
2021-08-08 22:49:13 +0200

    Still no tests but commented the code! Also chngs

--- contracts/LowSodiumWallet.sol
+++ contracts/LowSodiumWallet.sol
@@ -15,49 +15,81 @@ contract LowSodiumWallet {
      */
     struct PendingTransaction {
         address contractAddress;
-        uint64 maturity;
+        uint96 maturity;
 
-        uint256 amount;
         address destination;
+        uint96 ID;
+        uint256 amount;  
     }
 
-    // A blacklist of malicious addresses. Causes reversion of new transactions (but existing ones are fine)
-    mapping ( address => bool ) private blackList;
 
 
-    mapping ( uint => PendingTransaction ) private pendingTransactions;
+
+    // A map of ID->Tx.
+    mapping ( uint96 => PendingTransaction ) private pendingTransactions;
+
+    // A map of reserved money per denomination (address as proxy for denomination. 0x0 = Eth).
     mapping ( address => uint256 ) private reserved;
 
-    uint256 private nonce;
-    uint64 immutable private delay;
+    /**
+        Simplest counter. Why is it a uint96? Struct packing.
+
+        You can only perform 79228 trillion trillion transactions with this wallet, 
+        and after that your money will be locked down forever. Please do less than 
+        three trillion trillion transactions a day or it will run out in 72 years.
+
+        Why do I put the ID into the struct? Good qwestion, I don't know.
+     */
+    uint96 private nonce;
+
+    // All transactions have a pending period of -delay-.
+    uint96 immutable private delay;
+    // Owner address. Owner controls everything.
     address immutable private owner;
 
-    constructor(uint64 _delay, address _owner) {
-        nonce = 0;
-        delay = _delay;
-        owner = _owner;
 
-        blackList[address(this)] = true;
+
+
+    constructor(uint96 _delay) {
+
+        nonce = 0; // The first tx is #1 because the nonce pre-increments
+        delay = _delay;
+        owner = msg.sender;
+
     }
 
-    function orderTransaction(address _contractAddress, uint256 _amount, address _destination) external returns (uint256) {
+
+
+    /**
+
+        Register the pending tx if everything is alright.
+
+     */
+    function orderTransaction(address _contractAddress, uint256 _amount, address _destination) external returns (uint96) {
 
         require(msg.sender == owner);
 
-        require(!blackList[_destination]);
+        // Do not send money to itself
+        require(_destination != address(this));
 
+        // This block checks for available funds
         if(_contractAddress == address(0)) {
-            require(address(this).balance >= _amount + reserved[address(0)]);
+            require(address(this).balance >= _amount + reserved[address(0)]);  // Eth
         } else {
-            require(IERC20(_contractAddress).balanceOf(address(this)) >= _amount + reserved[_contractAddress]);
+            require(IERC20(_contractAddress).balanceOf(address(this)) >= _amount + reserved[_contractAddress]); // ERC20-compatible token
         }
 
         nonce++;
+
         PendingTransaction memory pt;
         pt.contractAddress = _contractAddress;
-        pt.maturity = uint64(block.timestamp) + delay;
-        pt.amount = _amount;
+        pt.maturity = uint96(block.timestamp) + delay;
+
         pt.destination = _destination;
+        pt.ID = nonce;
+
+        pt.amount = _amount;
+
         pendingTransactions[nonce] = pt;
         reserved[_contractAddress] += _amount;
 
@@ -65,24 +97,36 @@ contract LowSodiumWallet {
 
     }
 
-    function ammendDestination(uint256 _txId, address _destination) external {
+    /**
+        We refuse to ammend the destination if half the delay has passed,
+        to protect against some hypothetical last-second attack where some
+        actor who knows the pwd of the account waits for the user to send
+        a tx and changes the destination at the last moment.
+     */
+    function ammendDestination(uint96 _txId, address _destination) external {
 
         require(msg.sender == owner);
 
-        require(!blackList[_destination]);
+        // Do not send money to itself
+        require(_destination != address(this));
 
+        // Check the timestamp. If id is not correct, maturity will be = 0 and it will revert.
         require(pendingTransactions[_txId].maturity > block.timestamp + delay/2);
 
         pendingTransactions[_txId].destination = _destination;
 
     }
 
-    function cancelTransaction(uint256 _txId) external {
+    /**
+        Can be made at any point prior to maturity. Cancels the tx immediately and irreversibly.
+     */
+    function cancelTransaction(uint96 _txId) external {
 
         require(msg.sender == owner);
 
         PendingTransaction memory pt = pendingTransactions[_txId];
 
+        // Check the timestamp. If id is not correct, maturity will be = 0 and it will revert.
         require(pt.maturity > block.timestamp);
 
         reserved[pt.contractAddress] -= pt.amount;
@@ -90,18 +134,22 @@ contract LowSodiumWallet {
 
     }
 
-    function finishTransaction(uint256 _txId) external payable {
-
-        require(msg.sender == owner);
+    /**
+        Can be made by anyone at any point >= maturity. Executes the tx.
+     */
+    function finishTransaction(uint96 _txId) external payable {
 
         PendingTransaction memory pt = pendingTransactions[_txId];
 
-        require(pt.amount > 0);
-        require(pt.maturity < block.timestamp);
+        require(pt.amount > 0); // Checks that it exists.
+        require(pt.maturity <= block.timestamp);
 
         reserved[pt.contractAddress] -= pt.amount;
         delete(pendingTransactions[_txId]);
 
+        // Execution. Does not check for funds or specific conditions,
+        // so wierd ERC20s could make this fail. Imagine a contract that burns tokens
+        // everyday, or one that does not let you withdraw (ever).
         if(pt.contractAddress == address(0)) {
             payable(pt.destination).transfer(pt.amount);
         } else {
@@ -110,18 +158,6 @@ contract LowSodiumWallet {
 
     }
 
-    function addToBlacklist(address addr) external {
-        require(msg.sender == owner);
-
-        blackList[addr] = true;
-        
-    }
-
-    function removeFromBlacklist(address addr) external {
-        require(msg.sender == owner);
-
-        delete(blackList[addr]);
-        
-    }
+    // TODO events.
     
 }
